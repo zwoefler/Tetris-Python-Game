be74154defe3012e82887833e72c753a5aa484d9
Martin Fabian <martinfz@t-online.de>
2019-07-31 00:45:06 +0200

    Made local variables constants

--- scripts/tetris.py
+++ scripts/tetris.py
@@ -15,6 +15,13 @@ PLAY_HEIGHT = 600           # 600 // 20 = 30 height per block
 BLOCK_SIZE = 30
 DESCRIPTION_FONT = pygame.font.SysFont('Arial', int(BLOCK_SIZE * 0.5))
 
+# Preview Rectangle constants
+# Rectangle Positions
+X_POS_RECT = 0.1 * S_WIDTH
+Y_POS_RECT = 0.25 * S_HEIGHT
+# Rectangle dimensions
+RECT_WIDTH = PLAY_WIDTH * 0.3
+RECT_HEIGHT = RECT_WIDTH * 2
 
 # Global GRID Variables
 ROWS = 20
@@ -256,13 +263,6 @@ def draw_window(surface, grid):
 
 def draw_score_preview(surface, score_instance, next_shape):
     """Draws the rectangle to preview the next piece, show the score and the current level"""
-    # Rectangle Positions
-    x_pos_rect = 0.1 * S_WIDTH
-    y_pos_rect = 0.25 * S_HEIGHT
-    # Rectangle dimensions
-    rect_width = PLAY_WIDTH * 0.3
-    rect_height = rect_width * 2
-
     # Font settings
     score_font = DESCRIPTION_FONT.render("Score", 1, BLACK)
     level_font = DESCRIPTION_FONT.render("Level", 1, BLACK)
@@ -276,26 +276,26 @@ def draw_score_preview(surface, score_instance, next_shape):
     pygame.draw.rect(
         surface,
         GRAY_BORDER,
-        (x_pos_rect, y_pos_rect, rect_width, rect_height))
+        (X_POS_RECT, Y_POS_RECT, RECT_WIDTH, RECT_HEIGHT))
 
 
     # Print "Score" and dummy for the actual score
-    surface.blit(score_font, (x_pos_rect * 1.05, y_pos_rect * 1.05))
-    surface.blit(current_score_font, (x_pos_rect * 1.15, y_pos_rect * 1.15))
+    surface.blit(score_font, (X_POS_RECT * 1.05, Y_POS_RECT * 1.05))
+    surface.blit(current_score_font, (X_POS_RECT * 1.15, Y_POS_RECT * 1.15))
 
     # Print "Level" and a dummy for the current level
-    surface.blit(level_font, (x_pos_rect * 1.05, y_pos_rect * 1.25))
-    surface.blit(current_level_font, (x_pos_rect * 1.15, y_pos_rect * 1.35))
+    surface.blit(level_font, (X_POS_RECT * 1.05, Y_POS_RECT * 1.25))
+    surface.blit(current_level_font, (X_POS_RECT * 1.15, Y_POS_RECT * 1.35))
 
     # Print "Lines" and a dummy for cleared lines so far
-    surface.blit(lines_font, (x_pos_rect * 1.05, y_pos_rect * 1.45))
-    surface.blit(current_lines_font, (x_pos_rect * 1.15, y_pos_rect * 1.55))
+    surface.blit(lines_font, (X_POS_RECT * 1.05, Y_POS_RECT * 1.45))
+    surface.blit(current_lines_font, (X_POS_RECT * 1.15, Y_POS_RECT * 1.55))
 
     # Print "Next" as indication for the next comming piece
     f_width, _f_height = DESCRIPTION_FONT.size("Next")
-    surface.blit(preview_font, (x_pos_rect + f_width, y_pos_rect * 1.65))
+    surface.blit(preview_font, (X_POS_RECT + f_width, Y_POS_RECT * 1.65))
     # Aligning the preview shape beneeth the "Next" text
-    next_shape.draw_next_shape(WINDOW, x_pos_rect + f_width / 1.5, y_pos_rect * 1.75)
+    next_shape.draw_next_shape(WINDOW, X_POS_RECT + f_width / 1.5, Y_POS_RECT * 1.75)
 
 
 
